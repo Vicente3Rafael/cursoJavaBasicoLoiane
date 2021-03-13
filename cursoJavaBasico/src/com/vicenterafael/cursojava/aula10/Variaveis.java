aa717bbe86679303493c4a9bf14a47542d1d610d
Vicente Rafael <vicenterafael672@gmail.com>
2021-03-13 15:35:20 -0300

    primeiro projeto no netBeans

---
+++ cursoJavaBasico/src/com/vicenterafael/cursojava/aula10/Variaveis.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vicenterafael.cursojava.aula10;
+
+/**
+ *
+ * @author rafin
+ */
+public class Variaveis {
+    public static void main(String[] args){
+        int idade;
+        String nome;
+        String nomeDoMeuCachorro;
+        
+        idade = 23;
+        
+        System.out.println("idade: " + idade);
+    }
+}
